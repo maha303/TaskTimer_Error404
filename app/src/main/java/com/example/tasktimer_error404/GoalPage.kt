2182749ae7caa657f07575107e2436c6cd0f0f55
Linah-alkhurayyif <73877590+Linah-alkhurayyif@users.noreply.github.com>
2021-11-02 15:28:49 +0300

    add Goal Page

---
+++ app/src/main/java/com/example/tasktimer_error404/GoalPage.kt
@@ -0,0 +1,11 @@
+package com.example.tasktimer_error404
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class GoalPage : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_goal_page)
+    }
+}
