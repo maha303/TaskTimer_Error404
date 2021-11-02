f7653146d297462d6cb49cc1b09704af7c6828a3
Lubabah-mujallid <90623112+Lubabah-mujallid@users.noreply.github.com>
2021-11-02 22:05:02 +0300

    added data binding
    added data binding in goals details page + some fixes

--- app/src/main/java/com/example/tasktimer_error404/GoalDetailsPage.kt
+++ app/src/main/java/com/example/tasktimer_error404/GoalDetailsPage.kt
@@ -2,10 +2,20 @@ package com.example.tasktimer_error404
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.example.tasktimer_error404.databinding.ActivityMainBinding
 
 class GoalDetailsPage : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_goal_details_page)
+        initializeBinding()
+    }
+
+    private lateinit var binding: ActivityMainBinding
+    private fun initializeBinding() {
+        Log.d("MAIN", "Binding Initialized!")
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
