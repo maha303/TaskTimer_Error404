9c0db4d8d7d973c21f5fcac321999fb9844d1629
maha303 <86849690+maha303@users.noreply.github.com>
2021-11-03 17:27:21 +0300

    timerPageDone

--- app/src/main/java/com/example/tasktimer_error404/MainActivity.kt
+++ app/src/main/java/com/example/tasktimer_error404/MainActivity.kt
@@ -1,11 +1,18 @@
 package com.example.tasktimer_error404
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.constraintlayout.widget.ConstraintLayout
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val l = findViewById<ConstraintLayout>(R.id.llcon)
+        l.setOnClickListener {
+            val intt = Intent(this,TimerPage::class.java)
+            startActivity(intt)
+        }
     }
 }
--- app/src/main/java/com/example/tasktimer_error404/TimerPage.kt
+++ app/src/main/java/com/example/tasktimer_error404/TimerPage.kt
@@ -1,11 +1,82 @@
 package com.example.tasktimer_error404
 
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.tasktimer_error404.databinding.ActivityTimerPageBinding
+import java.util.*
+import kotlin.math.roundToInt
+
 
 class TimerPage : AppCompatActivity() {
+    private lateinit var binding: ActivityTimerPageBinding
+    private var timerStarted=false
+    private lateinit var serviceIntent:Intent
+    private var time=0.0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_timer_page)
+        binding = ActivityTimerPageBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.StartStopButton.setOnClickListener { startStopTimer() }
+        binding.resetButton.setOnClickListener { resetTimer() }
+
+        serviceIntent = Intent(applicationContext, TimerService::class.java)
+        registerReceiver(updateTime, IntentFilter(TimerService.TIME_UPDATED))
+
     }
+    private fun resetTimer() {
+        stopTimer()
+        time=0.0
+        binding.timeTv.text=getTimeStringFromDouble(time)
+
+    }
+
+    private fun startStopTimer() {
+        if(timerStarted)
+            stopTimer()
+             else
+                startTimer()
+    }
+
+    private fun startTimer() {
+        serviceIntent.putExtra(TimerService.TIME_EXTRA,time)
+        startService(serviceIntent)
+        binding.StartStopButton.text="Stop"
+        binding.StartStopButton.icon=getDrawable(R.drawable.pause_icon)
+        timerStarted=true
+    }
+
+    private fun stopTimer() {
+
+        stopService(serviceIntent)
+        binding.StartStopButton.text="Start"
+        binding.StartStopButton.icon=getDrawable(R.drawable.play_icon)
+        timerStarted=false
+    }
+
+    private val updateTime:BroadcastReceiver = object :BroadcastReceiver(){
+        override fun onReceive(context: Context, intent: Intent) {
+           time=intent.getDoubleExtra(TimerService.TIME_EXTRA,0.0)
+            binding.timeTv.text=getTimeStringFromDouble(time)
+        }
+    }
+
+    private fun getTimeStringFromDouble(time: Double): String
+    {
+        val resultInt = time.roundToInt()
+        val hours = resultInt % 86400 / 3600
+        val minutes = resultInt % 86400 % 3600 / 60
+        val seconds = resultInt % 86400 % 3600 % 60
+        return makeTimeString(hours,minutes,seconds)
+
+    }
+
+    private fun makeTimeString(hour: Int, min: Int, sec: Int): String = String.format("%02d:%02d:%02d",hour,min,sec)
+
+
 }
