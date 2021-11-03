137be4ab4d8d95a70ba0d461bd710d35d4118a2d
maha303 <86849690+maha303@users.noreply.github.com>
2021-11-03 17:28:38 +0300

    timerPageDone

---
+++ app/src/main/java/com/example/tasktimer_error404/TimerService.kt
@@ -0,0 +1,35 @@
+package com.example.tasktimer_error404
+
+import android.app.Service
+import android.content.Intent
+import android.os.IBinder
+import java.util.*
+
+class TimerService :Service() {
+    override fun onBind(intent: Intent?): IBinder? =null
+    private val timer = Timer()
+
+    override fun onStartCommand(intent: Intent, flags: Int, startId: Int): Int {
+        val time = intent.getDoubleExtra(TIME_EXTRA,0.0)
+        timer.scheduleAtFixedRate(TimeTask(time),0,1000)
+        return START_NOT_STICKY
+
+    }
+
+    override fun onDestroy() {
+        timer.cancel()
+        super.onDestroy()
+    }
+    private inner class TimeTask(private var time :Double):TimerTask(){
+        override fun run() {
+            val intent=Intent(TIME_UPDATED)
+            time++
+            intent.putExtra(TIME_EXTRA,time)
+            sendBroadcast(intent)
+        }
+    }
+    companion object{
+        const val TIME_UPDATED = "timerUpdated"
+        const val TIME_EXTRA="timeExtra"
+    }
+}
